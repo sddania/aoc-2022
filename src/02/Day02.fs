301ebc133d90f22028230c89c8a38dfca451a052
Daniele Agosti <daniele.agosti@ellycode.com>
2022-12-03 19:35:11 +0100

    added active pattern!

--- src/02/Day02.fs
+++ src/02/Day02.fs
@@ -29,35 +29,6 @@ module Day02 =
         | (Game.Scissor, Game.Scissor) -> Result.Draw
         | _ -> ArgumentOutOfRangeException() |> raise
         
-    let rockPaperScissorPart2 game1 game2 =
-        match (game2,game1) with
-        | (Game.Rock, Game.Rock) -> Result.Lose
-        | (Game.Rock, Game.Paper) -> Result.Lose
-        | (Game.Rock, Game.Scissor) -> Result.Lose
-        | (Game.Paper, Game.Rock) -> Result.Draw
-        | (Game.Paper, Game.Paper) -> Result.Draw
-        | (Game.Paper, Game.Scissor) -> Result.Draw
-        | (Game.Scissor, Game.Rock) -> Result.Win
-        | (Game.Scissor, Game.Paper) -> Result.Win
-        | (Game.Scissor, Game.Scissor) -> Result.Win
-        | _ -> ArgumentOutOfRangeException() |> raise
-    let parseGame game =
-        match game with
-        | 'A' | 'X' -> Game.Rock 
-        | 'B' | 'Y' -> Game.Paper 
-        | 'C' | 'Z' -> Game.Scissor
-        | _ -> ArgumentOutOfRangeException() |> raise
-    let parseLine line =
-        let chars = line |> Array.ofSeq
-        match chars with
-        | [|game1;_;game2|] -> (parseGame game1, parseGame game2)
-        | _ -> ArgumentOutOfRangeException() |> raise        
-    let calculatePoints (game: Match) =
-        let (game1, game2) = game
-        let resultGame = rockPaperScissor game1 game2
-        let resultPoints = int resultGame
-        let gamePoint = int game2
-        resultPoints + gamePoint        
     let gamePointFromResult (game,result) =
         match (result, game) with
         | (Result.Win, Game.Rock) -> Game.Paper
@@ -70,10 +41,35 @@ module Day02 =
         | (Result.Lose, Game.Paper) -> Game.Rock
         | (Result.Lose, Game.Scissor) -> Game.Paper
         | _ -> ArgumentOutOfRangeException() |> raise
+        
+    let translateGame2ToResultForPart2 game2 =
+        match game2 with
+        | Game.Rock -> Result.Lose
+        | Game.Paper -> Result.Draw
+        | Game.Scissor -> Result.Win
+        | _ -> ArgumentOutOfRangeException() |> raise
+        
+    let (|Game|_|) game =
+        match game with
+        | 'A' | 'X' -> Some Game.Rock 
+        | 'B' | 'Y' -> Some Game.Paper 
+        | 'C' | 'Z' -> Some Game.Scissor
+        | _ -> None
+    let parseLine line =
+        let chars = line |> Array.ofSeq
+        match chars with
+        | [|Game game1;_;Game game2|] -> (game1, game2)
+        | _ -> ArgumentOutOfRangeException() |> raise        
+    let calculatePoints (game: Match) =
+        let (game1, game2) = game
+        let resultGame = rockPaperScissor game1 game2
+        let resultPoints = int resultGame
+        let gamePoint = int game2
+        resultPoints + gamePoint        
     
     let calculatePoints2 (game: Match) =
         let (game1, game2) = game
-        let resultGame = rockPaperScissorPart2 game1 game2
+        let resultGame = translateGame2ToResultForPart2 game2
         let gamePoint = int (gamePointFromResult (game1,resultGame))
         let resultPoints = int resultGame
         resultPoints + gamePoint
